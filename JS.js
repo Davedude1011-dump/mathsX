6139bcf6e1d06e1e1b870a42dd1712453b7fd18c
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-06-22 16:53:19 +0100

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,138 @@
+function Aselected() {
+    document.getElementById("card").innerHTML = document.getElementById("1").innerHTML
+}
+function Bselected() {
+    document.getElementById("card").innerHTML = document.getElementById("2").innerHTML
+}
+function Cselected() {
+    document.getElementById("card").innerHTML = document.getElementById("3").innerHTML
+}
+function Dselected() {
+    document.getElementById("card").innerHTML = document.getElementById("4").innerHTML
+}
+function Eselected() {
+    document.getElementById("card").innerHTML = document.getElementById("5").innerHTML
+}
+function Fselected() {
+    document.getElementById("card").innerHTML = document.getElementById("6").innerHTML
+}
+function Gselected() {
+    document.getElementById("card").innerHTML = document.getElementById("7").innerHTML
+}
+function Hselected() {
+    document.getElementById("card").innerHTML = document.getElementById("8").innerHTML
+}
+function Iselected() {
+    document.getElementById("card").innerHTML = document.getElementById("9").innerHTML
+}
+
+function A1() {
+    document.getElementById("A1text").innerHTML = document.getElementById("card").innerHTML
+}
+function A2() {
+    document.getElementById("A2text").innerHTML = document.getElementById("card").innerHTML
+}
+function A3() {
+    document.getElementById("A3text").innerHTML = document.getElementById("card").innerHTML
+}
+function B1() {
+    document.getElementById("B1text").innerHTML = document.getElementById("card").innerHTML
+}
+function B2() {
+    document.getElementById("B2text").innerHTML = document.getElementById("card").innerHTML
+}
+function B3() {
+    document.getElementById("B3text").innerHTML = document.getElementById("card").innerHTML
+}
+function C1() {
+    document.getElementById("C1text").innerHTML = document.getElementById("card").innerHTML
+}
+function C2() {
+    document.getElementById("C2text").innerHTML = document.getElementById("card").innerHTML
+}
+function C3() {
+    document.getElementById("C3text").innerHTML = document.getElementById("card").innerHTML
+}
+
+function A1a() {
+    if (document.getElementById("A1text1").innerHTML === " ") {
+        document.getElementById("A1text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("A1text1").innerHTML
+        document.getElementById("A1text1").innerHTML = " "
+    }
+}
+function A2a() {
+    if (document.getElementById("A2text1").innerHTML === " ") {
+        document.getElementById("A2text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("A2text1").innerHTML
+        document.getElementById("A2text1").innerHTML = " "
+    }
+}
+function A3a() {
+    if (document.getElementById("A3text1").innerHTML === " ") {
+        document.getElementById("A3text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("A3text1").innerHTML
+        document.getElementById("A3text1").innerHTML = " "
+    }
+}
+function B1a() {
+    if (document.getElementById("B1text1").innerHTML === " ") {
+        document.getElementById("B1text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("B1text1").innerHTML
+        document.getElementById("B1text1").innerHTML = " "
+    }
+}
+function B2a() {
+    if (document.getElementById("B2text1").innerHTML === " ") {
+        document.getElementById("B2text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("B2text1").innerHTML
+        document.getElementById("B2text1").innerHTML = " "
+    }
+}
+function B3a() {
+    if (document.getElementById("B3text1").innerHTML === " ") {
+        document.getElementById("B3text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("B3text1").innerHTML
+        document.getElementById("B3text1").innerHTML = " "
+    }
+}
+function C1a() {
+    if (document.getElementById("C1text1").innerHTML === " ") {
+        document.getElementById("C1text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("C1text1").innerHTML
+        document.getElementById("C1text1").innerHTML = " "
+    }
+    
+}
+function C2a() {
+    if (document.getElementById("C2text1").innerHTML === " ") {
+        document.getElementById("C2text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("C2text1").innerHTML
+        document.getElementById("C2text1").innerHTML = " "
+    }
+}
+function C3a() {
+    if (document.getElementById("C3text1").innerHTML === " ") {
+        document.getElementById("C3text1").innerHTML = document.getElementById("card").innerHTML
+    }
+    else {
+        document.getElementById("card").innerHTML = document.getElementById("C3text1").innerHTML
+        document.getElementById("C3text1").innerHTML = " "
+    }
+}
